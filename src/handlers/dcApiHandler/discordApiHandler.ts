7b9f09483fee3e7ee3297fa5842b740201b105ca
Davi <al.davi.melo.aredes.costa@gmail.com>
2023-05-17 15:53:17 -0300

    discord api handler added

--- src/handlers/dcApiHandler/discordApiHandler.ts
+++ src/handlers/dcApiHandler/discordApiHandler.ts
@@ -1,9 +1,28 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const { APP_ID, APP_TOKEN } = process.env;
+
+async function sendRequestToApi (method: string, url: string, data?: unknown) {
+  let request: AxiosRequestConfig<any> = {};
+
+  request.url = url;
+  request.method = method;
+  request.headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bot ${APP_TOKEN}`
+  };
+  
+  if (data) {
+    request.data = data;
+  }
+
+  const res = await axios(request);
+
+  return res;
+}
 
 export class DiscordApiHandler {
   private baseUrl: string;
@@ -13,14 +32,15 @@ export class DiscordApiHandler {
   }
 
   public async post (target: string, data: unknown[]) {
-    const res = await axios({
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bot ${APP_TOKEN}`
-      },
-      data
-    });
+    const url = `${this.baseUrl}/commands`;
+    const res = await sendRequestToApi('post', url, data);
+
+    return res;
+  }
+
+  public async get (target: string) {
+    const url = `${this.baseUrl}/commands`;
+    const res = await sendRequestToApi('get', url);
 
     return res;
   }

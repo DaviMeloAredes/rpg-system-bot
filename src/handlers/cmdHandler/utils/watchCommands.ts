5e9ef77740cff363d457178e1c6899fd095d2f08
Davi <al.davi.melo.aredes.costa@gmail.com>
2023-05-20 15:28:32 -0300

    delete method added to dc api handler

--- src/handlers/cmdHandler/utils/watchCommands.ts
+++ src/handlers/cmdHandler/utils/watchCommands.ts
@@ -1,5 +1,17 @@
 import { Command } from '../../../interfaces/Command';
+import { DiscordApiHandler } from '../../dcApiHandler/discordApiHandler';
 
 export default async (files: Command[]) => {
-  
+  const dcApiHandler = new DiscordApiHandler();
+  const appCmds: any[] = await dcApiHandler.get('commands');
+
+  appCmds.forEach(async (cmd: any) => {
+    const filter = files.filter((f) => {
+      return f.name === cmd.name;
+    });
+
+    if (filter.length <= 0) {
+      await dcApiHandler.delete('commands', `${cmd.id}`);
+    }
+  });
 }
